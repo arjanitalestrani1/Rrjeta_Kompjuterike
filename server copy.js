d9244d9618869ccee80e5a687ce23cf3386e2c33
Fatjona-G <fatjona.gashi4@student.uni-pr.edu>
2026-04-15 23:53:55 +0200

    Added file command routing system (list, read, delete, search, info)

---
+++ server copy.js
@@ -0,0 +1,27 @@
+socket.on('data', (data) => {
+    const text = data.toString().trim();
+
+    const [cmd, ...args] = text.split(" ");
+
+    // KOMANDAT
+    if (cmd === "/list") {
+        commands.listFiles(socket);
+    } 
+    else if (cmd === "/read") {
+        commands.readFile(socket, args[0]);
+    } 
+    else if (cmd === "/delete") {
+        commands.deleteFile(socket, args[0]);
+    } 
+    else if (cmd === "/search") {
+        commands.searchFile(socket, args[0]);
+    } 
+    else if (cmd === "/info") {
+        commands.fileInfo(socket, args[0]);
+    } 
+    else {
+        socket.write("Komandë e panjohur.\n");
+    }
+
+    console.log(`Mesazh nga ${socket.remoteAddress}:${socket.remotePort} -> ${text}`);
+ });
